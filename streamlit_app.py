4c9ed33e28053788f7a821f305cd466293b03085
LMVic <141222711+LMVic@users.noreply.github.com>
2025-07-22 17:31:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).
-    Que pasa muñones!!!"
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/). Que pasa muñones!!!"
 )
