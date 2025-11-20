1771d97c2792c605222e6c228a24e8da236317a6
LMVic <141222711+LMVic@users.noreply.github.com>
2025-11-20 08:59:44 +0100

    Update streamlit_app.py
    Got rid of CAR  blue background color

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if uploaded_file:
 
         zone_colors = {
             "PAS": "white", "ISL": "white", "POD": "#A7C7E7",
-            "CDE": "#7EA7D7", "CTR": "#7EA7D7", "XSL": "#90EE90", "CAR": "#7EA7D7",
+            "CDE": "#7EA7D7", "CTR": "#7EA7D7", "XSL": "#90EE90",
             "CAJ": "#90EE90", "EXP": "#FFA07A", "IMP": "#90EE90", "ALT": "#FFA07A",
             "10": "#FFFF14",  "20": "#FFA500",  "30": "#A7C7E7",  "40": "#90EE90",  "50": "#FF00FF",  "PC": "white",
         }
